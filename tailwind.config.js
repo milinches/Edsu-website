93e3888253649b5997385b4b38bf9ce9db36fd1f
explorarthecreator <fredadejiemma99@gmail.com>
2021-11-12 16:52:50 +0100

    Removed the backgrong image

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,6 @@ module.exports = {
       colors:{
         primary:"#64DD17"
       },
-      backgroundImage:{
-        'programBG' :"url('../images/programBackground.png')",
-      },
       left:{
         // '1/6':20%
         // "1/7": '150px'
